948351bfba56e7285f7512d7bbebed06a2c7620d
djmary-k <kondratyukmv@gmail.com>
2023-08-27 04:11:46 +0400

    commit_02

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 # Завдання
 # У цьому домашньому завданні ми:
-# Додамо поле для дня народження Birthday. Це поле не обов'язкове, але може бути тільки одне.
-# Додамо функціонал роботи з Birthday у клас Record, а саме функцію days_to_birthday, яка повертає кількість днів до наступного дня народження.
-# Додамо функціонал перевірки на правильність наведених значень для полів Phone, Birthday.
+# +Додамо поле для дня народження Birthday. Це поле не обов'язкове, але може бути тільки одне.
+# +Додамо функціонал роботи з Birthday у клас Record, а саме функцію days_to_birthday, яка повертає кількість днів до наступного дня народження.
+# +Додамо функціонал перевірки на правильність наведених значень для полів Phone, Birthday.
 # Додамо пагінацію (посторінковий висновок) для AddressBook для ситуацій, коли книга дуже велика і треба показати вміст частинами, а не все одразу. Реалізуємо це через створення ітератора за записами.
 
 # Критерії прийому:
 # AddressBook реалізує метод iterator, який повертає генератор за записами AddressBook і за одну ітерацію повертає уявлення для N записів.
-# Клас Record приймає ще один додатковий (опціональний) аргумент класу Birthday
-# Клас Record реалізує метод days_to_birthday, який повертає кількість днів до наступного дня народження контакту, якщо день народження заданий.
-# setter та getter логіку для атрибутів value спадкоємців Field.
-# Перевірку на коректність веденого номера телефону setter для value класу Phone.
-# Перевірку на коректність веденого дня народження setter для value класу Birthday.
+# +Клас Record приймає ще один додатковий (опціональний) аргумент класу Birthday
+# +Клас Record реалізує метод days_to_birthday, який повертає кількість днів до наступного дня народження контакту, якщо день народження заданий.
+# +setter та getter логіку для атрибутів value спадкоємців Field.
+# +Перевірку на коректність веденого номера телефону setter для value класу Phone.
+# +Перевірку на коректність веденого дня народження setter для value класу Birthday.
 
 from collections import UserDict
 from datetime import datetime, timedelta
@@ -20,7 +20,7 @@ from datetime import datetime, timedelta
 class Field:
     def __init__(self, value):
         self.value = value
-        # print(f'from Field: {self.value}')
+        print(f'from Field: {self.value}')
 
 
 class Name(Field):
@@ -29,15 +29,39 @@ class Name(Field):
 
 
 class Phone(Field):
-    # phone = None
-    pass
+    def __init__(self, value):
+        self.__value = None
+        self.value = value
+        super().__init__(self.__value)
+    
+    @property
+    def value(self):
+        return self.__value
+    
+    @value.setter
+    def value(self, value):
+        # print(value.__class__)
+        if (type(value) == int) and (len(str(value)) == 12):
+            self.__value = value
+
 
 
 class Birthday(Field):
-    pass
-    # def __init__(self, date_bd):
-    #     # birthday in format: 22.09.2000
-    #     self.date_bd = date_bd
+    def __init__(self, value):
+        self.__value = None
+        self.value = value
+        super().__init__(self.__value)
+    
+    @property
+    def value(self):
+        return self.__value
+    
+    @value.setter
+    def value(self, value):
+        # print(value.__class__)
+        if type(value) == datetime:
+            self.__value = value
+
 
 class Record:
     def __init__(self, name: Name, phone: Phone=None, birthday: Birthday=None): #: phone - це не обовязковий агрумент, тому по замовчюванню None
@@ -71,13 +95,18 @@ class Record:
         self.phone[index] = new_phone
 
     def days_to_birthday(self, birthday): #повертає кількість днів до наступного дня народження
-        bd = Birthday(birthday)
+        # bd = Birthday(birthday) # birthday in format: 22.09.2000
         current_date = datetime.now().date()
-        if bd:
-            bd_date = 
-
-        pass
-
+        if birthday:
+            birthday_date = birthday.replace(year=datetime.now().year).date() 
+            delta = birthday_date - current_date
+            if delta.days >= 0:
+                return f'{delta.days} days to birthday'
+            else:
+                delta = birthday_date.replace(year=datetime.now().year+1) - current_date
+                return f'{delta.days} days to birthday'
+        else:
+            return None
 
 
 class AddressBook(UserDict):
@@ -98,13 +127,15 @@ class AddressBook(UserDict):
 # ### Checking mentor
 if __name__ == "__main__":
     name = Name('Bill')
-    phone = Phone('1234567890')
-    birthday = Birthday('22.09.2000')
+    phone = Phone(123456789012)
+    birthday = Birthday(datetime(1990, 8, 3))
     rec = Record(name, phone, birthday)
     ab = AddressBook()
     ab.add_record(rec)
     print(ab)
-    print(rec)
+    print(rec.birthday.value)
+    print(rec.days_to_birthday(datetime(1990, 8, 3)))
+    print(rec.phone)
     ##print(isinstance(ab['Bill'], Record))
     assert isinstance(ab['Bill'], Record) # оператор assert працює так, він порівнює щось, я якщо це порівняння дає false - він викликає помилку
     ##print(isinstance(ab['Bill'].name, Name))
@@ -114,6 +145,6 @@ if __name__ == "__main__":
     ##print(isinstance(ab['Bill'].phone[0], Phone))
     assert isinstance(ab['Bill'].phone[0], Phone)
     ##print(ab['Bill'].phone[0].value == '1234567890')
-    assert ab['Bill'].phone[0].value == '1234567890'
+    assert ab['Bill'].phone[0].value == 123456789012
     print('All Ok)')
 
